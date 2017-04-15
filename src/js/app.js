5ecf987978a7dd7f22784d0898a44c068b4c0add
Peter Hancock <peter.hancock@ocado.com>
2017-04-15 11:37:40 +0100

    material design

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,16 @@
 import html from 'choo/html'
 import choo from 'choo'
+import css from 'csjs'
+import cssify from 'cssify'
+require('bulma/css/bulma.css')
+cssify.byUrl('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')
+
+const style = css `
+.editor {
+  font-family: monospace;
+  font-size: inherit;
+}
+`
 
 const logger = (state, emitter) => {
   emitter.on('*', (messageName, data) => {
@@ -7,40 +18,40 @@ const logger = (state, emitter) => {
   })
 }
 
-const updateBalance = (state, emitter) => {
-  const balance = calculate(state.data)
-  const report = `
-Accounts:
+const updateBalance = (state) => {
+  let balance = null
+  try {
+    balance = calculate(state.data)
+    const report =
+  `${balance}
+  ----  input  ----
+  ${state.data}`
 
-${balance}
-
-
-----  input  ----
-${state.data}
-  `
-  state.report = report
-  emitter.emit('render')
+    state.report = report
+    state.error = null
+  } catch (e) {
+    state.report = null
+    state.error = e.message
+  }
 }
 
 const dataStore = (state, emitter) => {
-  state.data =
-`#  Account format:
-#    from to amount [ignored]
-#    e.g. bob alice 30.86 for the phone bill
-
-`
+  state.data = null
   state.report = null
+  state.error = null
   emitter.on('update', (data) => {
     state.data = data
     emitter.emit('render')
   })
-  emitter.on('generate', () => {
-    updateBalance(state, emitter)
+  emitter.on('refresh', () => {
+    updateBalance(state)
     emitter.emit('render')
   })
   emitter.on('send', () => {
-    updateBalance(state, emitter)
-    window.location = `mailto:?subject=Accounts&body=${encodeURIComponent(state.report)}`
+    updateBalance(state)
+    if (!state.error) {
+      window.location = `mailto:?subject=Accounts&body=${encodeURIComponent(state.report)}`
+    }
     emitter.emit('render')
   })
 }
@@ -65,28 +76,68 @@ const calculate = (raw) => {
   }, []).join('\n')
 }
 
-const mainView = (state, emit) => html`
-  <body>
-    <div style='display: flex; justify-content: space-around;'>
-      <textarea
-        style= "font-family: monospace;font-size: inherit;" type="text" rows="20" cols="100"
-        onchange=${(e) => { emit('update', e.target.value) }}>${state.data}</textarea>
-      <div>
-        ${state.report && html`<div><pre>${state.report}</pre></div>`}
+const on = (handle) => (e) => {
+  handle(e)
+  e.preventDefault()
+}
+
+const placeholder =
+`#  Account format:
+#    from to amount [ignored]
+#    e.g. bob alice 30.86 for the phone bill
+
+`
+
+const mainView = (state, emit) => html `
+<body>
+  <div class="content container is-fluid">
+    <h1>accounts</h1>
+    <div class="columns">
+      <div class="column">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              debts
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <textarea class='textarea ${style.editor}' type="text" rows="10" cols="60"  placeholder=${placeholder}
+                onchange=${(e) => { emit('update', e.target.value) }}>${state.data}</textarea>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <button class="button is-primary is-large card-footer-item" onclick=${on((e) => { emit('refresh') })}>
+              <span class="icon"><i class="fa fa-refresh"></i></span>\u00a0 refresh
+            </button>
+            <button class="button is-primary is-large card-footer-item" onclick=${on((e) => { emit('send') })}>
+              <span class="icon"><i class="fa fa-envelope"></i></span>\u00a0 send
+            </button>
+          </footer>
+        </div>
+      </div>
+      <div class="column">
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            actions
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            ${state.report && html`<pre>${state.report}</pre>`}
+            ${state.error && html`
+              <div class="notification is-danger">
+                Format error:
+                ${state.error}
+              </div>`
+            }
+          </div>
+        </div>
       </div>
     </div>
-    <div style='display: flex; justify-content: flex-start;'>
-      <button onclick=${(e) => {
-        emit('generate')
-        e.preventDefault()
-      }}>Generate</button>
-      <button onclick=${(e) => {
-        emit('send')
-        e.preventDefault()
-      }}>Send</button>
-    </div>
-
-  </body>
+  </div>
+</body>
 `
 const app = choo()
 app.use(logger)
